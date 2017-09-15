697128bee840ea0db7aeb3c5e634bcb834046f03
kalenpw <kalenpwilliams@gmail.com>
2017-09-15 15:25:19 -0600

    PhpStorm directory fixed

--- phpStorm.sh
+++ phpStorm.sh
@@ -2,4 +2,9 @@
 
 #Launches an instance of phpstorm
 
-/opt/PhpStorm-172.4155.25/bin/phpstorm.sh & disown
+pcname=$(hostname)
+if [ "$pcname" == "kalenpwT420s" ]; then
+    /opt/PhpStorm-172.4155.41/bin/phpstorm.sh & disown
+else
+    /opt/PhpStorm-172.4155.25/bin/phpstorm.sh & disown
+fi
