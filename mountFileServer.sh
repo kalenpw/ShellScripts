ad32bc6d2db1dfd00da83f21366642e3d7dbfee9
kalenpw <kalenpwilliams@gmail.com>
2017-09-21 20:06:43 -0600

    Mount Music as well

--- mountFileServer.sh
+++ mountFileServer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sshfs kalenpw@96.19.26.253:/media/kalenpw/Files /media/kalenpw/FileServer
+sshfs kalenpw@96.19.26.253:/media/kalenpw/Files /media/kalenpw/FileServer/
+
+sshfs kalenpw@96.19.26.253:/media/kalenpw/Music /media/kalenpw/Music/
