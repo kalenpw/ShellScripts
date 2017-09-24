90ec66df783e350e8352e8e84450c2c6066f5cc1
kalenpw <kalenpwilliams@gmail.com>
2017-09-23 22:55:35 -0600

    Add weather to i3status

---
+++ cmusRemote.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /media/kalenpw/HDD/Documents/Websites/CmusRemoteBrowser
+nodejs ./server.js & disown
--- i3statusBig.sh
+++ i3statusBig.sh
@@ -2,11 +2,12 @@
 
 i3status | while :
 do
+    weather=$(/home/kalenpw/Documents/ShellScripts/weather.sh)
     artist="$(cmus-remote -Q | sed -n -e 's/^.*tag artist //p')"
     song="$(cmus-remote -Q | sed -n -e 's/^.*tag title //p')"
 
     ram="$(awk '/MemTotal/ {memtotal=$2}; /MemAvailable/ {memavail=$2}; END { printf("%.0f", (100- (memavail / memtotal * 100))) }' /proc/meminfo)"
-    formatted="$artist - $song | RAM: $ram%"
+    formatted="$artist - $song | W: $weather | RAM: $ram%"
     read line
     echo "$formatted | $line" || exit 1
 
