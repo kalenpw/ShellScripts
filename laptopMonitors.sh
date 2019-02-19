6061590a58a29181341f8a4f016a8a1ef2677964
kalenpw <kalenpwilliams@gmail.com>
2019-02-19 11:01:03 -0700

    Laptop monitors script

---
+++ laptopMonitors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+work_monitor="HDMI1"
+internal_monitor="LVDS1"
+
+monitors=$(xrandr | grep " connected " | awk '{ print$1 }')
+#below only gets when activ
+
+#monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
+
+if [[ $monitors == *"$work_monitor"* ]]; then
+    xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --mode 1600x900 --pos 1920x632 --rotate normal --output VGA1 --off
+else
+    xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 0x0 --rotate normal --output VGA1 --off
+fi
+
