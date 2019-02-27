11b84532cabdd9045f9708431e2fce5714e1bdf3
kalenpw <kalenpwilliams@gmail.com>
2019-02-27 11:00:41 -0700

    Remove ram icon

--- i3statusBig.sh
+++ i3statusBig.sh
@@ -8,7 +8,7 @@ do
 
     ram="$(awk '/MemTotal/ {memtotal=$2}; /MemAvailable/ {memavail=$2}; END { printf("%.0f", (100- (memavail / memtotal * 100))) }' /proc/meminfo)"
     #formatted="$artist - $song | W: $weather | RAM: $ram%"
-    formatted="$artist - $song | \xF0\x9F\x90\x8F $ram%"
+    formatted="$artist - $song | RAM: $ram%"
     read line
     echo -e "$formatted | $line" || exit 1
 
