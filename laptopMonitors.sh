116babe0795ce27baaca73f479aaaf7b5165b19b
kalenpw <kalenpw@kalenpw.com>
2020-05-07 09:54:41 -0600

    New monitor changes

--- laptopMonitors.sh
+++ laptopMonitors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-work_monitor="HDMI-1"
-internal_monitor="LVDS-1"
+work_monitor="HDMI2"
+internal_monitor="eDP1"
 
 monitors=$(xrandr | grep " connected " | awk '{ print$1 }')
 #below only gets when activ
@@ -9,8 +9,8 @@ monitors=$(xrandr | grep " connected " | awk '{ print$1 }')
 #monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
 
 if [[ $monitors == *"$work_monitor"* ]]; then
-    xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS-1 --mode 1600x900 --pos 1920x632 --rotate normal --output VGA1 --off
-else
-    xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI-1 --off --output LVDS-1 --mode 1600x900 --pos 0x0 --rotate normal --output VGA1 --off
+    xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 312x1440 --rotate normal --output DP1 --off --output HDMI2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
+else 
+    xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 fi
 
