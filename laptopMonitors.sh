b3eedefd99df4c7abaa8f381725873856af75c6b
kalenpw <kalenpw@kalenpw.com>
2020-06-26 21:58:14 -0600

    New dock monitor

--- laptopMonitors.sh
+++ laptopMonitors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-work_monitor="HDMI2"
+work_monitor="DVI-I-1-1"
 internal_monitor="eDP1"
 
 monitors=$(xrandr | grep " connected " | awk '{ print$1 }')
@@ -9,8 +9,8 @@ monitors=$(xrandr | grep " connected " | awk '{ print$1 }')
 #monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
 
 if [[ $monitors == *"$work_monitor"* ]]; then
-    xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 312x1440 --rotate normal --output DP1 --off --output HDMI2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
+    xrandr --output DVI-I-1-1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 344x1440 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
 else 
-    xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+    xrandr --output DVI-I-1-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
 fi
 
