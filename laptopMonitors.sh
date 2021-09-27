f0d056ad0017cbdd86235bad90d725608105cb53
kalenpw <kalenpw@kalenpw.com>
2021-09-26 22:13:31 -0600

    Copy merged wallpaper to Pictures

--- laptopMonitors.sh
+++ laptopMonitors.sh
@@ -9,7 +9,7 @@ monitors=$(xrandr | grep " connected " | awk '{ print$1 }')
 #monitors=$(xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
 
 if [[ $monitors == *"$external_monitor"* ]]; then
-    gsettings set org.gnome.desktop.background picture-uri file:///home/kalenpw/.var/app/org.gabmus.hydrapaper/cache/hydrapaper/merged_wallpaper.png
+    gsettings set org.gnome.desktop.background picture-uri file:///home/kalenpw/Pictures/Wallpapers/merged.png
 else 
     gsettings set org.gnome.desktop.background picture-uri file:///home/kalenpw/Pictures/Wallpapers/taki.jpg
 fi
